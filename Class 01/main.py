3c8b012eb0bac70237bc71c9db4e97ce561b992d
Tuhin <www.mdtuhin4556@gmail.com>
2025-07-26 22:31:55 +0600

    first class

---
+++ Class 01/main.py
@@ -0,0 +1,70 @@
+# using print to output a message
+# print("Hello World")
+# string = "Hello World" or  'Hello World'
+# integer = 10
+# float = 10.5
+# boolean = True or False
+# multi_line_comment 
+""" This is a biggner course 
+from diu cpc """
+
+print(type(""" This is a biggner course 
+from diu cpc """ ))
+
+# variable declaration
+string   = "hello world"
+integer  = 10
+float    = 10.5
+boolean  = True
+
+# Print the variables
+print(string)
+print(integer)
+print(float)
+print(boolean)
+
+
+# Show the type of each variable
+print(type(string))
+print(type(integer))
+print(type(float))
+print(type(boolean))
+
+# why class
+# for type  the class  have come 
+
+# statement in python 
+if integer > 5 or integer == 5:
+    print("integer is greater than 5")
+elif integer == 5:
+    print("integer is equal to 5")
+else:
+    print("integer is less than 5")
+
+
+
+# for loop in python
+# range(start, stop-1)
+for i in range(0,5):
+    print(i)
+
+# while loop in python
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+    
+# where we use this :
+# 1. for loop is used when we know the number of iterations
+# 2. while loop is used when we don't know the number of iterations
+# Funtion in python
+# class in python
+# conditional statements in python
+
+
+# assignment on the topics
+# 1.Explore others data types in python
+# 2. Write a program to check if a number is even or odd
+# 3. Write a program to find the factorial of a number using for loop
+
+
